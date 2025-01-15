65735b2d5236be761d78099bb6411f2a9d3969f0
Mara <144994725+mara-dc@users.noreply.github.com>
2025-01-15 13:02:22 +0100

    Add files via upload

---
+++ 19-detention.py
@@ -0,0 +1,4 @@
+# Write code below 💖
+
+for x in range(100):
+  print('I will not use Snapchat in class')
---
+++ 20-99_bottles.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+for x in range(99, 0, -1):
+  print(f'{x} bottles of beer on the wall')
+  print(f'{x} bottles of beer')
+  print('Take one down, pass it around')
+  print(f'{x - 1} bottles of beer on the wall\n')
+
+print('No more bottles of beer on the wall,')
+print('no more bottles of beer.')
+print("We've taken them down")
+print('and passed them around;')
+print("now we're drunk and passed out!")
