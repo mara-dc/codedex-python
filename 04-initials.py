b284c4d82640885cdef55a127fbf4f9420fe849b
Mara <144994725+mara-dc@users.noreply.github.com>
2025-01-15 11:49:06 +0100

    Add files via upload

---
+++ 04-initials.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+# I'm trying
+print('M   M DDDD')
+print('MM MM D   D')
+print('MM MM D   D')
+print('M M M D   D')
+print('M   M D   D')
+print('M   M DDDD')
---
+++ 05-letter.py
@@ -0,0 +1,6 @@
+# Write code below 💖
+# I'm trying okay
+print('2024.07.08')
+print("I feel groggy from a 10-hr trip, but I'm trying to learn")
+print('Good job on sticking to your plan')
+print('🤌 🤌 🤌')
